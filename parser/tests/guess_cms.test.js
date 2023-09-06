10d8e274cbecd32fc83567511be69af38f5ee78a
g.sukhomlin@skillline.ru <g.sukhomlin@skillline.ru>
2023-09-06 11:11:35 +0200

    task/2916 - тесты для определения CMS

--- parser/tests/guess_cms.test.js
+++ parser/tests/guess_cms.test.js
@@ -1,81 +1,143 @@
 const Parser = require('../modules/parser');
 
-test('parser guess Bitrix', () => {
+const unipumpUrl = 'https://unipump.ru';
 
+test('parser guess Bitrix', async () => {
+    const parser = new Parser(unipumpUrl);
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('bitrix');
 });
 
-test('parser guess Wordpress', () => {
-
+test('parser guess Wordpress', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('wordpress');
 });
 
-test('parser guess Tilda', () => {
-
+test('parser guess Tilda', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('tilda');
 });
 
-test('parser guess Joomla', () => {
-
+test('parser guess Joomla', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('joomla');
 });
 
-test('parser guess OpenCart', () => {
-
+test('parser guess OpenCart', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('opencart');
 });
 
-test('parser guess Wix', () => {
-
+test('parser guess Wix', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('wix');
 });
 
-test('parser guess Shop-Script', () => {
-
+test('parser guess Shop-Script', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('shop-script');
 });
 
-test('parser guess DataLife Engine', () => {
-
+test('parser guess DataLife Engine', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('datalife engine');
 });
 
-test('parser guess Ucoz', () => {
-
+test('parser guess Ucoz', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('ucoz');
 });
 
-test('parser guess Nethouse', () => {
-
+test('parser guess Nethouse', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('nethouse');
 });
 
-test('parser guess Adobe Muse', () => {
-
+test('parser guess Adobe Muse', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('adobe muse');
 });
 
-test('parser guess UMI', () => {
-
+test('parser guess UMI', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('umi');
 });
 
-test('parser guess Drupal', () => {
-
+test('parser guess Drupal', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('drupal');
 });
 
-test('parser guess Okay', () => {
-
+test('parser guess Okay', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('okay');
 });
 
-test('parser guess PhpShop', () => {
-
+test('parser guess PhpShop', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('phpshop');
 });
 
-test('parser guess Amiro', () => {
-
+test('parser guess Amiro', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('amiro');
 });
 
-test('parser guess Netcat', () => {
-
+test('parser guess Netcat', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('netcat');
 });
 
-test('parser guess SiteEdit', () => {
-
+test('parser guess SiteEdit', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('siteedit');
 });
 
-test('parser guess ModX', () => {
-
+test('parser guess ModX', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('modx');
 });
 
-test('parser guess Magento', () => {
-
+test('parser guess Magento', async () => {
+    const parser = new Parser('');
+    const response = await parser.makeHttpsRequest(parser.getDomain());
+    const html = parser.getHtml(response);
+    expect(parser.guessCms(html)).toBe('magento');
 });
